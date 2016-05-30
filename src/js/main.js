c0d35c00d04d3ee1808fdead2dabe04c9fcfc821
karachevtsev <karachevtsev@yahoo.com>
2016-05-30 23:59:35 +0300

    Add daptivity for page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    console.log('Hello jQuery!')
+    var nav = $('.navigation');
+    // Responsive toggle navigation
+    $('.toggle-navigation').click(function() {
+        $(this).toggleClass('on');
+        $('.navigation').slideToggle();
+        return false;
+    });
+    $(window).resize(function(){
+        var wid = $(window).width();
+        if (wid > 992 && nav.is(":hidden")) {
+            nav.removeAttr('style');
+        }
+    });
+
 });

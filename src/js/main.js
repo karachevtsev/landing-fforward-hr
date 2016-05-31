5a6c786dd9519f5330e45b3a7b066dd6a4ea961c
karachevtsev <karachevtsev@yahoo.com>
2016-05-31 18:21:49 +0300

    Add sliders for comments and programs

--- src/js/main.js
+++ src/js/main.js
@@ -13,4 +13,50 @@ $(document).ready(function() {
         }
     });
 
+    $('.slider-comments').owlCarousel({
+        // loop: true,
+        margin: 10,
+        nav: true,
+        navText : "",
+        // autoplay: true,
+        fluidSpeed : 600,
+        autoplayHoverPause : true,
+        smartSpeed: 1000,
+        autoplayTimeout: 2000,
+        responsive: {
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('.slider-program').owlCarousel({
+        // loop: true,
+        margin: 10,
+        nav: true,
+        navText : "",
+        // autoplay: true,
+        fluidSpeed : 600,
+        autoplayHoverPause : true,
+        smartSpeed: 1000,
+        autoplayTimeout: 2000,
+        responsive: {
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
 });
